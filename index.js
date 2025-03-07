1accbde4edb9414242ba1c043a8e18bf86d7441c
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-07 19:34:57 +0300

    creates functions for Scuber's drivers(wrong way) to index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+
+function defaultCustomerName(){
+    return customerName;
+}
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    
+}
+
+
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+
+
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer = "can be bob";
+    changeLeastFavoriteCustomer = changeLeastFavoriteCustomer.toUpperCase();
+    
+}
+
+
+
